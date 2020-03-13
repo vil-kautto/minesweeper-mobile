00de7ba3cc318e36ee06d123a9da54ec06e84b5b
Ville Kautto <ville.kautto@tuni.fi>
2020-03-13 12:33:04 +0200

    add menu button handling
    added responses to menu button presses, also minor tweak to actionbar

--- app/src/main/java/fi/tuni/minesweeper/MainActivity.java
+++ app/src/main/java/fi/tuni/minesweeper/MainActivity.java
@@ -2,19 +2,39 @@ package fi.tuni.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Activity messenger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        messenger = this;
     }
 
-    //handles button click events on main menu
+    // handles button click events on main menu
     public void clicked(View v) {
+        switch(v.getId()) {
+            case(R.id.startButton):
+                toaster("Play");
+                break;
+            case(R.id.highScoreButton):
+                toaster("High Scores");
+                break;
+            case(R.id.settingsButton):
+                toaster("Settings");
+                break;
+        }
 
+    }
+    public void toaster(String text) {
+        Toast.makeText(messenger, text, Toast.LENGTH_LONG).show();
     }
 }

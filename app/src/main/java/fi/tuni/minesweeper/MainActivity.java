b38c87f407464568e0d5deedcb100362a042826b
Ville Kautto <ville.kautto@tuni.fi>
2020-03-13 11:49:45 +0200

    add main menu buttons
    added buttons to main menu

--- app/src/main/java/fi/tuni/minesweeper/MainActivity.java
+++ app/src/main/java/fi/tuni/minesweeper/MainActivity.java
@@ -3,6 +3,7 @@ package fi.tuni.minesweeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //handles button click events on main menu
+    public void clicked(View v) {
+
     }
 }
